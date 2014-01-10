6cef8a9e55c4f3dfd2bfc148e6c0b1e5ffd94493
Evan <eg1290@gmail.com>
2014-01-09 18:05:02 -0600

    removed compiled files.

--- PPC32diab/CORERobot_partialImage/Debug/Objects/CORERobot/CORERobot/CORERobot.d
+++
@@ -1,9 +0,0 @@
-CORERobot_partialImage/Debug/Objects/CORERobot/CORERobot/CORERobot.o: \
-  C:/Users/core2062/offseason/CORERobot/CORERobot/CORERobot.cpp \
-  C:\WindRiver\diab\5.4.0.0\include\diab\lpragma.h \
-  C:\WindRiver\diab\5.4.0.0\include\diab\xmacros.h
-
-C:\WindRiver\diab\5.4.0.0\include\diab\lpragma.h:
-
-C:\WindRiver\diab\5.4.0.0\include\diab\xmacros.h:
-
